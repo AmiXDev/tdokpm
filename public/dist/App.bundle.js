c083db62a7045b820426584dd2a5b69adbfb838f
Amixengineer <34276557+Amixengineer@users.noreply.github.com>
2020-08-16 18:09:23 +0430

    web sharing api 2

--- public/dist/App.bundle.js
+++ public/dist/App.bundle.js
@@ -11287,7 +11287,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 /* 3 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: \n@import 'partials/layout';\n^\n      File to import not found or unreadable: /Volumes/Drive/github/tdokpm/public/sass/partials/_layout.scss.\n      in /Volumes/Drive/github/tdokpm/public/sass/style.scss (line 2, column 1)\n    at /Volumes/Drive/github/tdokpm/node_modules/webpack/lib/NormalModule.js:192:19\n    at /Volumes/Drive/github/tdokpm/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Volumes/Drive/github/tdokpm/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/Volumes/Drive/github/tdokpm/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.callback (/Volumes/Drive/github/tdokpm/node_modules/sass-loader/lib/loader.js:57:13)\n    at Object.<anonymous> (/Volumes/Drive/github/tdokpm/node_modules/sass-loader/node_modules/async/dist/async.js:2271:31)\n    at Object.callback (/Volumes/Drive/github/tdokpm/node_modules/sass-loader/node_modules/async/dist/async.js:969:16)\n    at options.error (/Volumes/Drive/github/tdokpm/node_modules/node-sass/lib/index.js:294:32)");
+// removed by extract-text-webpack-plugin
 
 /***/ }),
 /* 4 */
